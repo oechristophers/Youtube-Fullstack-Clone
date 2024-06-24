79eaee286b2cbd392397f1aadd72ae711361d8b2
oechristophers <oechristophers2020@gmail.com>
2024-06-24 11:56:04 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  publicDir: 'public',
-  build: {
-    rollupOptions: {
-      input: 'public/index.html'
-    }
-  }
+ 
 });
